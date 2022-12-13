3e184bd6ce53dae492e33982e06bef5ea6b80127
gptmarker <ozgur@altogic.com>
2022-12-13 14:10:51 +0300

    button problem fixed

--- content/content.js
+++ content/content.js
@@ -3,40 +3,30 @@ import altogic from '../src/libs/altogic';
 const container = document.querySelector('form > :first-child > :first-child');
 const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="18" width="18"><path fill='currentColor' d="M10 42V8.75q0-1.2.9-2.1.9-.9 2.1-.9h22q1.2 0 2.1.9.9.9.9 2.1V42l-14-6Zm3-4.55 11-4.65 11 4.65V8.75H13Zm0-28.7h22-11Z"/></svg>`;
 
-const btn = `<button id='saveBtnFromExtension' class="btn flex gap-2 justify-center btn-neutral">${svg} Save Thread</button>`;
-
-container?.classList?.add('button-parent-for-extension');
-let CSS = `<style id='CSSFromExtension'>`;
-CSS += `
-#saveBtnFromExtension {
-	display: none;
-}
-.button-parent-for-extension:has(button:not(#saveBtnFromExtension)) #saveBtnFromExtension { 
-	display: flex;
-}
-`;
-CSS += `</style>`;
-
-if (!document.querySelector('#CSSFromExtension')) {
-	document.head.insertAdjacentHTML('beforeend', CSS);
-}
+const btn = `<button id='saveThreadButtonFromGPTMarker' class="btn flex gap-2 justify-center btn-neutral">${svg} Save Thread</button>`;
 
 if (!document.querySelector('#saveBtnFromExtension')) {
 	container.insertAdjacentHTML('afterbegin', btn);
 }
 
-document.querySelector('#saveBtnFromExtension')?.addEventListener('click', saveThread);
+document.querySelector('#saveThreadButtonFromGPTMarker')?.addEventListener('click', saveThread);
 
 async function saveThread() {
-	const CONTAINER_SELECTOR = '[class*="react-scroll-to-bottom"] > [class*="react-scroll-to-bottom"] > :first-child';
-	const conversationThread = document.querySelector(CONTAINER_SELECTOR);
+	const SUBMIT_BUTTON = document.querySelector('form textarea + button');
+	const CONTAINER_SELECTOR =
+		'[class*="react-scroll-to-bottom"] > [class*="react-scroll-to-bottom"] > :first-child > *';
+	const conversationThread = document.querySelectorAll(CONTAINER_SELECTOR);
 	const threads = [];
+
+	if (SUBMIT_BUTTON.disabled) {
+		return alert('Please wait for the answer');
+	}
 
 	try {
 		this.disabled = true;
 		const { question, answer } = getFirstQuestionAndAnswer();
 
-		for (const child of conversationThread.children) {
+		for (const child of conversationThread) {
 			const markdownContainer = child.querySelector('.markdown');
 			if (child.classList.contains('dark:bg-gray-800')) {
 				threads.push({ from: 'human', content: child.textContent });
